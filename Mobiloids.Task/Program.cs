0c9c17eea3bbbe27c72f7ebe1fa1195eac9a4f34
Any Parsyan <parsyanany98@gamil.com>
2021-06-01 17:22:41 +0400

    Add ThreeBishops and Tetris tasks

--- Mobiloids.Task/Program.cs
+++ Mobiloids.Task/Program.cs
@@ -19,8 +19,18 @@ namespace Mobiloids.Task
             //int[] numbers = new int[]{2, 3, 5, 4, 3, 10, 13, 12};
             //Console.WriteLine(LaserStrike.SmallestPrime(numbers));
             #endregion
-
-
+            #region ThreeBishops
+            //Console.WriteLine(ThreeBishops.MinMoves(1,3,1,5,6,8));
+            //Console.WriteLine(ThreeBishops.MinMoves(1, 1, 1, 2, 1, 3));
+            #endregion
+            #region Tetris
+            //int[,] board = new int[6,5] { {1,1,1,1,1 },{0,0,1,1,1 },{ 1,1,1,1,1},
+            //                                {0,0,1,1,1 },{ 0,0,0,1,0},{0,0,0,0,0} };
+            //Tetris.Print(board,6,5);
+            //Console.WriteLine();
+            //Tetris.MoveDown(board, 6,5);
+            //Tetris.Print(board, 6,5);
+            #endregion
             Console.Read();
         }
 
---
+++ Mobiloids.Task/Tetris.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Mobiloids.Task
+{
+    class Tetris
+    {
+        public static void MoveDown(int[,] board, int height, int width)
+        {
+            int count = 0;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (board[i, j] == 1)
+                        count++;
+                    else
+                        break;
+                }
+                if (count == width)
+                {
+                    for (int x = i; x < height - 1; x++)
+                    {
+                        for (int y = 0; y < width; y++)
+                            board[x, y] = board[x + 1, y];
+                    }
+                    for (int x = height-1; x < height; x++)
+                    {
+                        for (int y = 0; y < width; y++)
+                            board[x, y] = 0;
+                    }
+                    count = 0;
+                    i--;
+                }
+                else
+                    count = 0;
+            }
+        }
+
+        public static void Print(int[,] board, int height, int width)
+        {
+            for (int  i = height-1;  i >= 0;  i--)
+            {
+                for (int y = 0; y < width; y++)
+                {
+                    Console.Write($"{board[i,y]}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
